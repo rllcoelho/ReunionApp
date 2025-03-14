2ec0321e78de01a914d1e1fcf0c59f1b1fd826b3
Rafael Coelho <rafallcoelho@gmail.com>
2025-03-13 22:11:08 -0300

    add logo to home title

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,6 @@
 import { Tabs,  } from 'expo-router'
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { Image } from 'react-native';
 
 export default function TabLayout() {
     return (
@@ -7,17 +8,23 @@ export default function TabLayout() {
             screenOptions={{
                 tabBarActiveTintColor: '#ffd33d',
                 headerStyle: {
-                backgroundColor: '#25292e',
+                backgroundColor: '#fff',
                 },
                 headerShadowVisible: false,
                 headerTintColor: '#fff',
                 tabBarStyle: {
-                backgroundColor: '#25292e',
+                backgroundColor: '#fff',
                 }
             }}
         >
             <Tabs.Screen name="index" options={{ 
-                title: 'Home',
+                headerTitle: () => (
+                    <Image
+                        style={{ width: 200, height: 200 }}
+                        source={require('@/assets/images/logo_roxo.png')}
+                        resizeMode="contain"
+                    />
+                ),
                 tabBarIcon: ({ color, focused }) => (
                     <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
                 )
